36d79cdd5a2e29e3ba50315e4ebe564f9e83dad0
Monthinkpad <jose.montalvo@uvltd.tech>
2022-09-28 13:56:00 -0600

    Revert "added .gitignore file and custom replies"
    This reverts commit d192cdf00697a1d18e35f060d4565e724f8317c3.

--- bot.py
+++ bot.py
@@ -4,35 +4,19 @@
 from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
 from botbuilder.schema import ChannelAccount
 import requests
-import json
 
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
-        
         # await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
-        message = str(turn_context.activity.text)
+        message = str(turn_context.activity.text).lower()
         response = ""
         if message == "test":
             response = "This is an automated test reply"
-        elif message == "APM":
+        elif message == "apm":
             response = "Great choice"
-        elif "cx" or "Cx" in message:
-        # elif message == "cx" or message == "Cx" or message == "ApmBot Cx" or message == "ApmBot cx":
-            wd_g = 'http://torpbookmonwb1.sandals.com/data.cfc?method=getDailyBookings&rsvType=WD'
-            data = requests.get(wd_g)
-            json_data = json.loads(data.text)
-            last = len(json_data) - 1
-            response = (f"Counts:\n+ Bookings Today: {json_data[last]['today']}\n+ Bookings Yesterday: {json_data[last]['yesterday']}\n+ Last Week: {json_data[last]['dayLastWeek']}\n+ Last Year: {json_data[last]['dayLastYear']}")
-        elif message == "today":
-            wd_g = 'http://torpbookmonwb1.sandals.com/data.cfc?method=getDailyBookings&rsvType=WD'
-            data = requests.get(wd_g)
-            json_data = json.loads(data.text)
-            last = len(json_data) - 1
-            response = (f"Bookings Today: {json_data[last]['today']}")
         else:
-            print(message)
             response = "Unknown command"
         return await turn_context.send_activity(
             MessageFactory.text(response)
